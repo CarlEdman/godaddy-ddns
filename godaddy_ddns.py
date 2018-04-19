c5043b9fd91e27804b11ca51d96d1fb1b2e6e248
Carl Edman <CarlEdman@gmail.com>
2018-04-19 10:24:35 -0400

    Update godaddy_ddns.py
    Conforming changes.

--- godaddy_ddns.py
+++ godaddy_ddns.py
@@ -1,144 +1,64 @@
-#!/usr/bin/env python3
-#
-# Update GoDaddy DNS "A" Record.
-#
-# usage: godaddy_ddns.py [-h] [--version] [--ip IP] [--key KEY]
-#                        [--secret SECRET] [--ttl TTL]
-#                        hostname
-#
-# positional arguments:
-#   hostname         DNS fully-qualified host name with an 'A' record.  If the hostname consists of only a domain name
-#                    (i.e., it contains only one period), the record for '@' is updated.
-#
-# optional arguments:
-#   -h, --help       show this help message and exit
-#   --version        show program's version number and exit
-#   --ip IP          DNS Address (defaults to public WAN address from http://ipv4.icanhazip.com/)
-#   --key KEY        GoDaddy production key
-#   --secret SECRET  GoDaddy production secret
-#   --ttl TTL        DNS TTL.
-#
-# GoDaddy customers can obtain values for the KEY and SECRET arguments by creating a production key at
-# https://developer.godaddy.com/keys/.
-#
-# Note that command line arguments may be specified in a FILE, one to a line, by instead giving
-# the argument "%FILE".  For security reasons, it is particularly recommended to supply the
-# KEY and SECRET arguments in such a file, rather than directly on the command line:
-#
-# Create a file named, e.g., `godaddy-ddns.config` with the content:
-#   MY.FULLY.QUALIFIED.HOSTNAME.COM
-#   --key
-#   MY-KEY-FROM-GODADDY
-#   --secret
-#   MY-SECRET-FROM-GODADDY
-#
-# Then just invoke `godaddy-ddns %godaddy-ddns.config`
+# godaddy-ddns
 
-prog='godaddy-ddns'
-version='0.3'
-author='Carl Edman (CarlEdman@gmail.com)'
+Introduction
+------------
+A script for dynamically updating a GoDaddy DNS record. I use GoDaddy to host DNS for a domain and I wanted to point an A record in that domain to a host who's IP address changes occasionally. GoDaddy has an API to do this, so this happened.  This package, in particular this README.MD, is inspired by the equivalent for CloudFlare offered by [thatjpk](https://github.com/thatjpk/cloudflare-ddns).
 
-import sys, json, argparse
+Dependencies
+------------
+This program was written and tested using Python 3.4, so you'll need:
 
-if sys.version_info > (3,):
-  from urllib.request import urlopen, Request
-  from urllib.error import URLError, HTTPError
-else:
-  from urllib2 import urlopen, Request
-  from urllib2 import URLError, HTTPError
+ - Python (`opkg install python3-light`):
+ - Codecs (`opkg install python3-codecs`)
+ - Email (`opkg install python3-email`)
+ - OpenSSL (`opkg install python3-openssl`)
 
-parser = argparse.ArgumentParser(description='Update GoDaddy DNS "A" Record.', fromfile_prefix_chars='%', epilog= \
-'''GoDaddy customers can obtain values for the KEY and SECRET arguments by creating a production key at
-https://developer.godaddy.com/keys/.
+The program may also work under Python 2.7 by changing python3 to python in the first line, but this configuration has not
+been tested.
 
-Note that command line arguments may be specified in a FILE, one to a line, by instead giving
-the argument "%FILE".  For security reasons, it is particularly recommended to supply the
-KEY and SECRET arguments in such a file, rather than directly on the command line.''')
+Usage
+-----
+Invoke the program like this:
 
-parser.add_argument('--version', action='version',
-  version='{} {}'.format(prog, version))
+     godaddy_ddns.py [-h] [--version] [--ip IP] [--key KEY] [--secret SECRET] [--ttl TTL] hostname
 
-parser.add_argument('hostname', type=str,
-  help='DNS fully-qualified host name with an A record.  If the hostname consists of only a domain name (i.e., it contains only one period), the record for @ is updated.')
+     positional arguments:
 
-parser.add_argument('--ip', type=str, default=None,
-  help='DNS Address (defaults to public WAN address from http://ipv4.icanhazip.com/)')
+     hostname         DNS fully-qualified host name with an 'A' record.  If the hostname consists of only a domain name
+                      (i.e., it contains only one period), the record for '@' is updated.
 
-parser.add_argument('--key', type=str, default='',
-  help='GoDaddy production key')
+     optional arguments:
 
-parser.add_argument('--secret', type=str, default='',
-  help='GoDaddy production secret')
+     -h, --help       show this help message and exit
 
-parser.add_argument('--ttl', type=int, default=3600 , help='DNS TTL.')
-args = parser.parse_args()
+     --version        show program's version number and exit
 
-def main():
-  hostnames = args.hostname.split('.')
+     --ip IP          DNS Address (defaults to public WAN address from http://ipv4.icanhazip.com/)
 
-  if len(hostnames)<2:
-    msg = 'Hostname "{}" is not a fully-qualified host name of form "HOST.DOMAIN.TOP".'.format(args.hostname)
-    raise Exception(msg)
-  elif len(hostnames)<3:
-    hostnames.insert(0,'@')
+     --key KEY        GoDaddy production key
 
-  if not args.ip:
-    try:
-      with urlopen("http://ipv4.icanhazip.com/") as f: resp=f.read()
-      if sys.version_info > (3,): resp = resp.decode('utf-8')
-      args.ip = resp.strip()
-    except URLError:
-      msg = 'Unable to automatically obtain IP address from http://ipv4.icanhazip.com/.'
-      raise Exception(msg)
+     --secret SECRET  GoDaddy production secret
 
-  ips = args.ip.split('.')
-  if len(ips)!=4 or \
-    not ips[0].isdigit() or not ips[1].isdigit() or not ips[2].isdigit() or not ips[3].isdigit() or \
-    int(ips[0])>255 or int(ips[1])>255 or int(ips[2])>255 or int(ips[3])>255:
-    msg = '"{}" is not valid IP address.'.format(args.ip)
-    raise Exception(msg)
+     --ttl TTL        DNS TTL.
 
-  url = 'https://api.godaddy.com/v1/domains/{}/records/A/{}'.format('.'.join(hostnames[1:]),hostnames[0])
-  data = json.dumps([ { "data": args.ip, "ttl": args.ttl, "name": hostnames[0], "type": "A" } ])
-  if sys.version_info > (3,):  data = data.encode('utf-8')
-  req = Request(url, method='PUT', data=data)
+GoDaddy customers can obtain values for the KEY and SECRET arguments by creating a production key at https://developer.godaddy.com/keys/.
 
-  req.add_header("Content-Type","application/json")
-  req.add_header("Accept","application/json")
-  if args.key and args.secret:
-    req.add_header("Authorization", "sso-key {}:{}".format(args.key,args.secret))
+Note that command line arguments may be specified in a FILE, one to a line, by instead giving the argument "@FILE".  For security reasons, it is particularly recommended to supply the KEY and SECRET arguments in such a file, rather than directly on the command line:
 
-  try:
-    with urlopen(req) as f: resp = f.read()
-    if sys.version_info > (3,):  resp = resp.decode('utf-8')
-    # resp = json.loads(resp)
-  except HTTPError as e:
-    if e.code==400:
-      msg = 'Unable to set IP address: GoDaddy API URL ({}) was malformed.'.format(req.full_url)
-    elif e.code==401:
-      if args.key and args.secret:
-        msg = '''Unable to set IP address: --key or --secret option incorrect.
-Correct values can be obtained from from https://developer.godaddy.com/keys/ and are ideally placed in a % file.'''
-      else:
-        msg = '''Unable to set IP address: --key or --secret option missing.
-Correct values can be obtained from from https://developer.godaddy.com/keys/ and are ideally placed in a % file.'''
-    elif e.code==403:
-        msg = '''Unable to set IP address: customer identified by --key and --secret options denied permission.
-Correct values can be obtained from from https://developer.godaddy.com/keys/ and are ideally placed in a % file.'''
-    elif e.code==404:
-        msg = 'Unable to set IP address: {} not found at GoDaddy.'.format(args.hostname)
-    elif e.code==422:
-        msg = 'Unable to set IP address: "{}" has invalid domain or lacks A record.'.format(args.hostname)
-    elif e.code==429:
-        msg = 'Unable to set IP address: too many requests to GoDaddy within brief period.'
-    else:
-      msg = 'Unable to set IP address: GoDaddy API failure because "{}".'.format(e.reason)
-    raise Exception(msg)
-  except URLError as e:
-    msg = 'Unable to set IP address: GoDaddy API failure because "{}".'.format(e.reason)
-    raise Exception(msg)
+Create a file named, e.g., `godaddy-ddns.config` with the content:
 
-  print('IP address for {} set to {}.'.format(args.hostname,args.ip))
+     MY.FULLY.QUALIFIED.HOSTNAME.COM
+     --key
+     MY-KEY-FROM-GODADDY
+     --secret
+     MY-SECRET-FROM-GODADDY
 
-if __name__ == '__main__':
-  main()
+Then just invoke `godaddy-ddns %godaddy-ddns.config`
+
+Credits and Thanks
+------------------
+ - [thatjpk](https://github.com/thatjpk/cloudflare-ddns) for providing an example of this type of script.
+ - [GoDaddy](https://www.godaddy.com/) for having an [API](https://developer.godaddy.com/).
+ - [icanhazip.com](http://icanhazip.com/) for making grabbing your public IP
+    from a script super easy.
+ - [dhowdy](https://github.com/dhowdy) for supplying a fix to problem with updating root DNS record.
